79d60520f95f108af18ea641020e630f618d8565
MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>
2023-03-02 11:50:09 -0300

    Update App.vue
    Signed-off-by: MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="slogan">
     <div class="text-center">
-      <h1>FUNCIONAAA</h1>
+      <h1>TESTE DE INTEGRAÇÃO COM O AGENTE SW</h1>
       <br/>
-      <h6 class="text-secondary">nao faco ideia do que to fazendo</h6>
+      <h6 class="text-secondary">teste para comunicação com agente skywalking</h6>
     </div>
   </div>
   <div id="main">
       <div class="container">
         <div class="row">
           <div class="col-md">
-            <h5>sei la</h5>
+            <h5>coluna</h5>
             <div class="card">
                <div class="card-body">
                 <ul class="list-group">
@@ -25,7 +25,7 @@
           </div>
 
           <div class="col-md">
-            <h5>sei la2</h5>
+            <h5>coluna 2</h5>
               <div class="card">
                 <div class="card-body">
                   <ul class="list-group">
@@ -41,7 +41,7 @@
         </div>
         <br/>
 
-        <h5>nao etnendo nada ainda</h5>
+        <h5>coluna 3</h5>
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
@@ -57,11 +57,22 @@
 </template>
 
 <script>
+import ClientMonitor from "skywalking-client-js";
 
 import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
 export default {
   name: 'app',
+  mounted() {
+        ClientMonitor.register({
+                collector: "10.0.28.209:8081",
+                service: "sky-client-teste",
+                vue: true,
+                pagePath: "root/teste/dist/",
+                enableSPA: true,
+                serviceVersion: "v1.0.0",
+        });
+},
   data: function (){
     return {
       prefixes: ['aaa', 'bbb', 'ccc'],
