36279e771bddbd235ed8dd25dbdeee5556343993
MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>
2023-03-02 11:49:23 -0300

    Update server.js
    Signed-off-by: MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+/*const {default: agent} = require('skywalking-backend-js');
+agent.start({
+        serviceName: 'service name',
+        serviceInstance: 'service instance',
+        collectorAddress: '10.0.28.209:8081',
+});*/
+
 const express = require('express');
 const app = express(),
-      bodyParser = require("body-parser");
+      bodyParser = require('body-parser');
       port = 8088;
-
-// place holder for the data
+//place holder for the data
 app.use(bodyParser.json());
 app.use(express.static(process.cwd() + '/dist'));
 
-
 app.get('/', (req,res) => {
-  res.sendFile(process.cwd() + '/dist/index.html');
+        res.sendFile(process.cwd() + '/dist/index.html');
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+        console.log(`Server listening on port:: ${port}`);
 });
