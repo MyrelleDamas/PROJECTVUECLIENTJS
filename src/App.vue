b8db8f288051f4c82abe6f98d760f049830d847d
MyrelleDamas <107486021+MyrelleDamas@users.noreply.github.com>
2025-11-06 13:18:52 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,9 @@ import CommentSection from './components/CommentSection.vue'
 import ClientMonitor from 'skywalking-client-js';
 
 ClientMonitor.register({
-<<<<<<< HEAD
-    collector: 'http://IPDOSERVIDOR:12888',
-    service: 'PROJETOSHELLOWORLD::FRONTEND-VUE',
-    pagePath: 'root/PROJECTVUECLIENTJS/dist',
-=======
     collector: 'http://IPDOSERVER:12800', // IP do servidor do Skywalking com porta que aponta para o ClientJS
     service: 'NOMEDOGRUPO::NOMEDOSERVICO', 
     pagePath: '/dist', // referenciar a pasta dist
->>>>>>> 889776bb3a4c0b80b52ba923a5b76c0e0a437aad
     serviceVersion: 'v2.0.0',
 });
 </script>
